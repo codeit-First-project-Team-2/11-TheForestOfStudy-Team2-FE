90dd0a75d4d2d9ceb54d7ab7281e08b2885667b0
Minsung Kim <alstjddl0513@gmail.com>
2026-01-27 20:31:12 +0900

    style:타이머 애니메이션 효과

--- src/constants/time.js
+++ src/constants/time.js
@@ -1,1 +1,1 @@
-export const INITIAL_SECONDS = 15; //테스트용.  기본 = 1500; 
+export const INITIAL_SECONDS = 1500; //테스트용.  기본 = 1500;
--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 
-import App from './App.jsx'
+import App from './App.jsx';
 
-import './styles/reset.css'
-import './styles/fonts.css'
-import './styles/index.css'
-
+import './styles/reset.css';
+import './styles/fonts.css';
+import './styles/index.css';
+import { BrowserRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>,
-)
+);
