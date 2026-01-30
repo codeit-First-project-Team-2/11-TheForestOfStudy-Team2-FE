9d7cce6045c4767973b4027a228c5611938c18c8
yyejin00 <bywlslgnsl20@gmail.com>
2026-01-30 18:19:33 +0900

    fix : 주석추가작성

--- src/apis/studyService.js
+++ src/apis/studyService.js
@@ -2,10 +2,14 @@
  * 📌 파일 작성 규칙
  * - 담당하신 API 파트에 본인 이름을 작성하고 로직을 완성해주세요.
  * - client(axios 인스턴스)를 사용하여 요청을 보냅니다.
+ * - 에러메시지는 constants에 상수로 사용합니다. 
+ * - 기능에 따라 추가해야할 에러메시지 상수도 constants에서 작성해주세요.
  * - 각 함수는 비동기(async/await)로 작성하며, 데이터 반환 형식은 response.data를 기본으로 합니다.
  */
 
+import { STUDY_ERROR_MESSAGES } from '../constants/error';
 import client from './client';
+
 
 // 1. 스터디 목록 조회 (GET /)
 // 담당: 000
