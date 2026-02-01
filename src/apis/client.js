381631d7bfdf0fb773c56cf85c7bd7fcde82282b
yyejin00 <bywlslgnsl20@gmail.com>
2026-02-01 16:28:06 +0900

    rename: apis 폴더이름 api로 변경

--- src/apis/client.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-console.log('현재 API 주소:', import.meta.env.VITE_API_URL);
-const client = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
-});
-
-export default client;
