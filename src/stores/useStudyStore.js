be9ccb9c5479da3f847182135ae524ab3bd2b467
yyejin00 <bywlslgnsl20@gmail.com>
2026-02-02 10:13:56 +0900

    feat:zustand사용해서 store작성

---
+++ src/stores/useStudyStore.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { create } from 'zustand';
+
+const useStudyStore = create((set) => ({
+  studyData: null,
+  isAuthenticated: false, //비밀번호 인증 성공여부
+  error: null,
+
+  setStudyData: (data) =>
+    set({ studyData: data, isAuthenticated: true, error: null }),
+
+  setEmojiStats: (newStats) =>
+    set((state) => {
+      if (!state.studyData) return state; //이모지가 null일때 방어코드
+
+      return {
+        studyData: {
+          ...state.studyData,
+          emojiStats: newStats,
+        },
+      };
+    }),
+
+  clearStudy: () =>
+    set({ studyData: null, isAuthenticated: false, error: null }),
+
+  setError: (message) => set({ error: message }),
+}));
+export default useStudyStore;
