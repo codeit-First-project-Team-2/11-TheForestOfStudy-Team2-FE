d264d5f70f2466d544a5f6eb2c517571954045e5
yyejin00 <bywlslgnsl20@gmail.com>
2026-02-02 18:50:22 +0900

    fix : 모달부분 조건부렌더링해서 삭제와 구분

--- src/pages/StudyDetail/StudyDetail.jsx
+++ src/pages/StudyDetail/StudyDetail.jsx
@@ -5,33 +5,40 @@ import { HabitsTable } from '../../components/HabitsTable/HabitsTable.jsx';
 import { EmojiAddition } from '../../components/EmojiAddition/EmojiAddition.jsx';
 import pointImg from '../../assets/studyDetail/Group.jpg';
 import { useNavigate, useParams } from 'react-router';
-import {
-  getStudyDetail,
-  verifyStudyPassword,
-} from '../../apis/studyService.js';
+import { deleteStudy, verifyStudyPassword } from '../../apis/studyService.js';
 import { Modal } from '../../components/ui/Modal/Modal.jsx';
 import useStudyStore from '../../stores/useStudyStore.js';
+import { TextField } from '../../components/ui/TextField/TextField.jsx';
+import { Button } from '../../components/ui/Button/Button.jsx';
+import toast from 'react-hot-toast';
 //todo 1.스타일 적용안하는 className 삭제 2. 컴포넌트 분리 3.발바닥컬러 습관id를기준으로 변경
 export const StudyDetail = () => {
   const { studyId } = useParams();
   const nav = useNavigate();
 
-  const { setStudyData, setError } = useStudyStore();
+  const { setStudyData, setError, clearStudy } = useStudyStore();
 
   //모달
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isShowPassword, setIsShowPassword] = useState(false);
   const [targetPath, setTargetPath] = useState(''); //비번 인증 성공 시 해당 path 저장
   const [password, setPassword] = useState('');
+  const [isDeleteConfirm, setIsDeleteConfirm] = useState(false);
 
   const handleProtectedAction = (type) => {
     const pathMap = {
       edit: `/studies/${studyId}/write`,
-      delete: 'delete', // 삭제는 이동이 아니라 함수 실행일 수 있으니 키워드만
+      delete: 'delete', // 삭제는 이동이 아니라 함수 실행이니까 키워드만
       habit: `/studies/${studyId}/habits`,
       focus: `/studies/${studyId}/focus`,
     };
     setTargetPath(pathMap[type]);
     setIsModalOpen(true);
+    if (type === 'delete') {
+      setIsDeleteConfirm(true);
+    } else {
+      setIsDeleteConfirm(false);
+    }
   };
 
   const handleCloseModal = () => {
@@ -44,14 +51,17 @@ export const StudyDetail = () => {
       const data = await verifyStudyPassword(studyId, password);
       setStudyData(data);
       if (targetPath === 'delete') {
-        //delete 로직 작성
+        deleteStudy(studyId, password);
+        toast.success('스터디가 성공적으로 삭제되었습니다.');
+        clearStudy(); //삭제후 스토어 초기화.
+        nav('/');
       } else {
         nav(targetPath);
       }
       handleCloseModal();
     } catch (error) {
       setError(error.message);
-      alert(error.message || '비밀번호가 틀렸습니다.');
+      toast.error(error.response?.data?.message || '비밀번호가 틀렸습니다.');
     }
   };
   //study가 undefined인 경우 보여줄 내용
@@ -112,24 +122,30 @@ export const StudyDetail = () => {
                 <Modal
                   isOpen={isModalOpen}
                   onClose={handleCloseModal}
-                  title="비밀번호 확인"
+                  title={study.title}
                   size="sm"
                 >
-                  <div className={styles.modalContent}>
-                    <p>권한이 필요합니다. 비밀번호를 입력해주세요.</p>
-                    <input
-                      type="password"
+                  {isDeleteConfirm ? (
+                    <div className={styles.alertContainer}>
+                      <div>⚠️스터디를 삭제하면 복구할 수 없습니다.</div>
+                    </div>
+                  ) : (
+                    ''
+                  )}
+                  <div className={styles.modalContainer}>
+                    <div>
+                      <p className={styles.modalLabelWrapper}>
+                        권한이 필요해요!
+                      </p>
+                    </div>
+                    <TextField
+                      type={isShowPassword ? 'text' : 'password'}
                       value={password}
                       onChange={(e) => setPassword(e.target.value)}
                       placeholder="비밀번호 입력"
-                      className={styles.passwordInput} // CSS는 예진님이 살짝 손봐주세요!
                     />
-                    <button
-                      onClick={handleConfirmPassword}
-                      className={styles.confirmBtn}
-                    >
-                      확인
-                    </button>
+
+                    <Button onClick={handleConfirmPassword}>확인</Button>
                   </div>
                 </Modal>
               </div>
