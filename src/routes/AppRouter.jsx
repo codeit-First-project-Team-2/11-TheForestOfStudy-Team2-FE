cfb1611bcb21a2de10756c5c0b8c32398ca2547c
yyejin00 <bywlslgnsl20@gmail.com>
2026-02-03 15:30:30 +0900

    init : home 파일정리 및 코드정리

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -3,6 +3,7 @@ import { UiPreview } from '../pages/UiPreview';
 import { TodayHabitPage } from '@/pages/TodayHabitPage';
 import { TodayFocus } from '../pages/TodayFocus';
 import { StudyDetail } from '../pages/StudyDetail/StudyDetail';
+import Home from '../pages/Home/home';
 
 // 주석처리된 Routes들을 꺼내서 쓰면 됩니다.
 // 페이지 import도 해주세요~!
@@ -10,11 +11,12 @@ export const AppRouter = () => {
   return (
     <Routes>
       {/* UI 테스트용 */}
+      <Route path="/" element={<Home />} />
       <Route path="/studies/:studyId" element={<StudyDetail />} />
       <Route path="/studies/:studyId/focus" element={<TodayFocus />} />
       <Route path="/studies/:studyId/habits" element={<TodayHabitPage />} />
       <Route path="*" element={<UiPreview />} />
-      {/* <Route path="/" element={<Home />} />
+      {/* 
       <Route path="/write" element={<StudyForm />} />
       <Route path="/studies/:studyId" element={<StudyDetail />} />
       */}
