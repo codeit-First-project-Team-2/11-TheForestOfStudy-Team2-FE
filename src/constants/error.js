dcbd661fb8cf3db1611ef876268d061f0d8033e9
yyejin00 <bywlslgnsl20@gmail.com>
2026-02-04 09:11:46 +0900

    feat : 홈부분 컴포넌트 수정 및 api 작업

--- src/constants/error.js
+++ src/constants/error.js
@@ -2,5 +2,6 @@ export const TOAST = {
   SUCCESS_DELETE: '스터디가 성공적으로 삭제되었습니다.',
   PASSWORD_INVALID: '비밀번호가 틀렸습니다.',
   EMOJI_ERROR: '이모지 반영 실패',
-  STUDY_ERROR: '스터디 반영 실패'
+  STUDY_ERROR: '스터디 반영 실패',
+  STUDIES_LOAD_ERROR :'스터디 로드 실패'
 };
